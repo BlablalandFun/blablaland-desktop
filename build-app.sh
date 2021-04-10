ed82dde288d9f4709f73e7d157e7d0ebbeb69a4a
Quentin <31007601+Yovach@users.noreply.github.com>
2021-04-10 20:36:41 +0200

    feat: ajout appveyor + fix qql trucs

--- build-app.sh
+++ build-app.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker run --rm -ti \
  --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') \
  --env ELECTRON_CACHE="/root/.cache/electron" \
@@ -7,4 +9,4 @@ docker run --rm -ti \
  -v ~/.cache/electron:/root/.cache/electron \
  -v ~/.cache/electron-builder:/root/.cache/electron-builder \
  electronuserland/builder:wine \
- /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux --win"
+ /bin/bash -c "yarn && yarn release"
