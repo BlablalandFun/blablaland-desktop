ea32678ec03493adf85a3c41d6ffa97c7e013d69
Quentin <31007601+Yovach@users.noreply.github.com>
2021-06-13 18:07:22 +0200

    feat: formatting code

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,42 @@
-const cp = require('child_process');
-const gulp = require('gulp');
+const cp = require("child_process");
+const gulp = require("gulp");
 
 const buildReact = (cb) => {
-    cp.exec("npm run react-build", (err, out) => {
-        if (err) {
-            console.error(err.message);
-        }
+  cp.exec("npm run react-build", (err, out) => {
+    if (err) {
+      console.error(err.message);
+    }
 
-        cb();
-    });
-    // cb();
+    cb();
+  });
+  // cb();
 };
 
 const copyPkg = (cb) => {
-    gulp.src('./package.json').pipe(gulp.dest('./build/'));
+  gulp.src("./package.json").pipe(gulp.dest("./build/"));
 
-    cb();
+  cb();
 };
 
 const buildElectron = (cb) => {
-    cp.exec("npm run electron-build", (err, out) => {
-        if (err) {
-            console.error(err.message);
-        }
+  cp.exec("npm run electron-build", (err, out) => {
+    if (err) {
+      console.error(err.message);
+    }
 
-        cb();
-    });
+    cb();
+  });
 };
 
 const releaseElectron = (cb) => {
-    cp.exec("npm run release", (err, out) => {
-        if (err) {
-            console.error(err.message);
-        }
+  cp.exec("npm run release", (err, out) => {
+    if (err) {
+      console.error(err.message);
+    }
 
-        cb();
-    });
+    cb();
+  });
 };
-
 
 exports.build = gulp.series(buildReact, copyPkg, buildElectron);
 exports.release = gulp.series(buildReact, copyPkg, releaseElectron);
